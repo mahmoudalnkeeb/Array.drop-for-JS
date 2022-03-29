9124c36babbb12ed79d1279ca33050d6761d5119
mahmoudalnkeeb <ramadanalnkeeb2018@gmail.com>
2022-03-29 15:41:52 +0200

    3 ways

--- drop.js
+++ drop.js
@@ -1,8 +1,18 @@
 Array.prototype.drop = function (val = 1) {
-    for (let i = 0; i < val; i++) {
-       val == 0 ? this :  this.shift();
-    }
+  for (let i = 0; i < val; i++) {
+    val == 0 ? this : this.shift();
+  }
   return this;
+
+  // return this.slice(val);
+
+  // this.splice(0, val);
+  // return this;
 };
 
-console.log(['oldest', 'older', 'old', 'new'].drop(1)); // drop ['oldest', 'older'] and return ['old', 'new']
+console.log(['oldest', 'older', 'old', 'new'].drop(0));
+console.log(['oldest', 'older', 'old', 'new'].drop(1));
+console.log(['oldest', 'older', 'old', 'new'].drop(2));
+console.log(['oldest', 'older', 'old', 'new'].drop(3));
+console.log(['oldest', 'older', 'old', 'new'].drop(4));
+console.log(['oldest', 'older', 'old', 'new'].drop(5));
